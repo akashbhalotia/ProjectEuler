3c447995b6880e8109b9204adc585279e341a403
Akash Bhalotia <akashbhalotia10@gmail.com>
2018-12-31 15:06:44 +0530

    Sum of even Fibonacci numbers that do not exceed 4e6
    *Simple looping

---
+++ P2.java
@@ -0,0 +1,23 @@
+// Created by akashbhalotia
+import java.io.*;
+import java.util.*;
+class A
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int i,a=1,b=1,c=a+b,sum=0;
+
+        while(c<=4000000)
+        {
+            if((c&1)==0)
+                sum+=c;
+
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        System.out.println(sum);
+    }
+}
